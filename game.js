a913b5cd3a4b5feaadfaedee8cb7165b993468b8
mustafa <aahash69@harrisonburg.k12.va.us>
2019-09-17 13:34:15 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var score = 0;
 
 postToPage(score); // Print to the page
 
-createButton('Button', btnPress); // Make a button
+createButton('Game', btnPress); // Make a button
 changeTitle('Clicker Game');
 
 setInterval(btnPress, 1000);
@@ -12,3 +12,16 @@ function btnPress() {
   postToPage(score);
 }
 
+var score = 0;
+
+postToPage(score); // Print to the page
+
+createButton('click', btnPress); // Make a button
+changeTitle('Clicker Game');
+
+setInterval(btnPress, 1000);
+
+function btnPress() {
+  score++;
+  postToPage(score);
+}
